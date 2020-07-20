6d911306a7a39bbdd443b73ac64cf7347f25aca4
shadab28 <32225089+shadab28@users.noreply.github.com>
2020-07-20 23:26:41 +0530

    colour change

--- app/src/main/java/com/example/favour/AddFavour.kt
+++ app/src/main/java/com/example/favour/AddFavour.kt
@@ -1,19 +1,16 @@
 package com.example.favour
-
 import android.app.Activity
-import android.app.Activity.*
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.Path
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_favour_request.*
-import com.example.favour.REQUEST_CODE as REQUEST_CODE1
 
-private const val REQUEST_CODE=42
+//private const val REQUEST_CODE=42
 class AddFavour: AppCompatActivity() {
     var iscolor = true
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,38 +19,35 @@ class AddFavour: AppCompatActivity() {
         BackButtonToHome.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
-        textView4.setOnClickListener(object : View.OnClickListener {
+        GroceriesButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
 
                 if (iscolor) {
-                    textView4.setBackgroundColor(Color.parseColor("#377118"))
-                    textView4.setTextColor(Color.WHITE)
+                    GroceriesButton.setBackgroundColor(Color.parseColor("#377118"))
+                    GroceriesButton.setTextColor(Color.WHITE)
                 } else {
-                    textView4.setBackgroundColor(Color.parseColor("#FFF"))
-                    textView4.setTextColor(Color.BLACK)
+                    GroceriesButton.setBackgroundColor(Color.parseColor("#FFF"))
+                    GroceriesButton.setTextColor(Color.BLACK)
                 }
                 iscolor = !iscolor
             }
         })
-    }
-}
-//
-//        PlaceFavourRequest.setOnClickListener {
-//            startActivity(Intent(this, MainActivity::class.java))
-//        }
 //
 //        OpenCamera.setOnClickListener {
+//            val TakePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 //
-//            startActivityForResult(takePictureIntent, REQUEST_CODE)
+//            startActivityForResult(TakePictureIntent, REQUEST_CODE)
+//
 //        }
 //    }
-//}
+//
 //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if (requestCode = com.example.favour.REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-//            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) as Bitmap
-//            ImageView.setImageBitmap(takenImage)
-//        } else {
-//            super.onActivityResult(requestCode, resultCode, data) as Bitmap
+//
+//        if (requestCode== REQUEST_CODE && resultCode==Activity.RESULT_OK){
+//            val TakenImage = data?.extras?.get("data") as Bitmap
+//            imageView.setImageBitmap(TakenImage)
+//        }else{
+//            super.onActivityResult(requestCode, resultCode, data)
 //        }
-//    }
-//}
+     }
+}
