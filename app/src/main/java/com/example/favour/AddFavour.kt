ddefd598821814c8105e801a3702163d285841c4
shadab28 <32225089+shadab28@users.noreply.github.com>
2020-07-22 06:00:22 +0530

    Camera permission and integration

--- app/src/main/java/com/example/favour/AddFavour.kt
+++ app/src/main/java/com/example/favour/AddFavour.kt
@@ -8,10 +8,11 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_favour.*
-
-//private const val REQUEST_CODE=42
+private const val REQUEST_CODE=42
+@Suppress("PLUGIN_WARNING")
 class AddFavour: AppCompatActivity() {
     var iscolor = true
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,36 +23,37 @@ class AddFavour: AppCompatActivity() {
         }
         PlaceFavourRequest.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
-        }
-//          switch1.setOnClickListener({_, })
 
-
-        GroceriesButton.setOnClickListener {
-            if (iscolor) {
-                GroceriesButton.setBackgroundColor(Color.parseColor("#377118"))
-                GroceriesButton.setTextColor(Color.WHITE)
-            } else {
-                GroceriesButton.setBackgroundColor(Color.parseColor("#FFF"))
-                GroceriesButton.setTextColor(Color.BLACK)
-            }
-            iscolor = !iscolor
         }
-//
-//        OpenCamera.setOnClickListener {
-//            val TakePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//
-//            startActivityForResult(TakePictureIntent, REQUEST_CODE)
-//
+        val sw = findViewById<Switch>(R.id.switch1)
+        sw?.setOnCheckedChangeListener { _, isChecked ->
+            val msg = if (isChecked) "Yes" else "No"
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            sw.text = msg
+        }
+//        GroceriesButton.setOnClickListener {
+//            if (iscolor) {
+//                GroceriesButton.setBackgroundColor(Color.parseColor("#377118"))
+//                GroceriesButton.setTextColor(Color.WHITE)
+//            } else {
+//                GroceriesButton.setBackgroundColor(Color.parseColor("#FFF"))
+//                GroceriesButton.setTextColor(Color.BLACK)
+//            }
+//            iscolor = !iscolor
 //        }
-//    }
 //
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//
-//        if (requestCode== REQUEST_CODE && resultCode==Activity.RESULT_OK){
-//            val TakenImage = data?.extras?.get("data") as Bitmap
-//            imageView.setImageBitmap(TakenImage)
-//        }else{
-//            super.onActivityResult(requestCode, resultCode, data)
-//        }
+        OpenCamera.setOnClickListener {
+            val TakePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(TakePictureIntent, REQUEST_CODE)
+            textView5.visibility= View.GONE
+            OpenCamera.visibility=View.GONE
+        }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) =
+        if (requestCode== REQUEST_CODE && resultCode==Activity.RESULT_OK){
+            val TakenImage = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(TakenImage)
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
 }
--- app/src/main/java/com/example/favour/MainActivity.kt
+++ app/src/main/java/com/example/favour/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.favour
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageItemInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupPermissions()
         AddFavourRequest.setOnClickListener {
             startActivity(Intent(this, AddFavour::class.java))
         }
@@ -26,11 +34,7 @@ class MainActivity : AppCompatActivity() {
             val adapter = PagerAdapter(supportFragmentManager)
             viewPager.adapter = adapter
 //        tabLayout.setupWithViewPager(viewPager)
-
-
             viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-
-
             tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabReselected(tab: TabLayout.Tab?) {
                     viewPager.currentItem = tab!!.position
@@ -44,5 +48,28 @@ class MainActivity : AppCompatActivity() {
 
             })
         }
+// CAMERA SETUP
+// https://pranaybhalerao.wordpress.com/2018/02/11/run-time-permission-in-androidkotlin/
 
+    private val CAMERA_REQUEST_CODE=123;
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if (permission!=PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
     }
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),CAMERA_REQUEST_CODE)
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
